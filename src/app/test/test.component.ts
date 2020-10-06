d712e3d1384dbbe1f4bb5332955998b00941bbdc
Pete Bacon Darwin <pete@bacondarwin.com>
2020-10-06 09:15:01 +0100

    add runtime translations

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  @Input()
+  label: string;
+
+  ngOnInit(): void {
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { enableProdMode } from '@angular/core';
+import { loadTranslations } from '@angular/localize';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+loadTranslations({
+  '33a8faca74df927c30c40c8db2679cc485a9f524': 'c\`est un test',
+});
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +15,4 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
